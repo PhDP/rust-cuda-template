26b16620b3a72dee378db204a53549d222295eca
Philippe Desjardins-Proulx <philippe.desjardins.proulx@umontreal.ca>
2022-10-30 03:25:12 -0400

    fix

--- build.rs
+++ build.rs
@@ -13,6 +13,7 @@ fn main() {
     println!("cargo:rustc-link-search=native=/usr/local/cuda/lib64/stubs");
     println!("cargo:rustc-link-search=native=lib");
     println!("cargo:rustc-link-lib=cudart");
+    println!("cargo:rustc-link-lib=cublas");
     println!("cargo:rustc-link-lib=cuda");
-    println!("cargo:rustc-link-lib=static=ruda");
 }
+
